a9a61a6f9dad7a79fdbc6d8cc68d51ec5712e446
cchristie <cchristie@generaldigital.com>
2022-12-14 13:03:36 -0500

    Fix FAKE context issue
    FAKE context must be created before any tasks are. As such, I have moved
    task initialization into a function that is called after the context is
    initialized. This will also allow the availability of command line
    parameters during the configuration of Tasks in the future.

--- Build/build.fs
+++ Build/build.fs
@@ -4,10 +4,11 @@ open Fake.IO
 open Fake.IO.FileSystemOperators
 open Fake.IO.Globbing.Operators
 open BlackFox.Fake
+open Fake.Core.Context
 
 let mutable dotNetOptions = fun _ -> DotNet.Options.Create()
 
-let slnDir = Path.getFullName "../"
+let slnDir = __SOURCE_DIRECTORY__ @@ "../" |> Path.getFullName
 let projName = "Feliz.React.Msal"
 let projDir =  slnDir @@ projName
 let paketLockFile = slnDir @@ "paket.lock"
@@ -19,58 +20,63 @@ let printRes (operation : string) (res : ProcessResult) =
         let concat = String.concat "; "
         failwith $"{operation} failed: {System.Environment.NewLine}{res.Errors |> concat}{System.Environment.NewLine}"
 
-let installDeps = BuildTask.create "InstallDependencies" [] {
-    dotNetOptions <- DotNet.install (fun opts ->
-        { opts with
-            Channel = DotNet.CliChannel.LTS})
+let initTargets () =
+    let installDeps = BuildTask.create "InstallDependencies" [] {
+        dotNetOptions <- DotNet.install (fun opts ->
+            { opts with
+                Channel = DotNet.CliChannel.LTS})
 
-    DotNet.exec dotNetOptions "tool" "restore" |> printRes "Tool restore"
+        DotNet.exec dotNetOptions "tool" "restore" |> printRes "Tool restore"
 
-    let paketCmd =
-        match File.exists paketLockFile with
-        | true -> "restore"
-        | false -> "install"
+        let paketCmd =
+            match File.exists paketLockFile with
+            | true -> "restore"
+            | false -> "install"
 
-    DotNet.exec dotNetOptions "paket" paketCmd |> printRes "Paket install"
+        DotNet.exec dotNetOptions "paket" paketCmd |> printRes "Paket install"
 
-    DotNet.exec dotNetOptions "femto" $"{projDir} --resolve"  |> printRes "Femto package resolution"
-}
+        DotNet.exec dotNetOptions "femto" $"{projDir} --resolve"  |> printRes "Femto package resolution"
+    }
 
-let clean = BuildTask.create "Clean" [] {
-    !! (projDir @@ "/**/bin")
-    ++ (projDir @@ "/**/obj")
-    |> Shell.cleanDirs
+    let clean = BuildTask.create "Clean" [] {
+        !! (projDir @@ "/**/bin")
+        ++ (projDir @@ "/**/obj")
+        |> Shell.cleanDirs
 
-    !! (projDir @@ "/**/*.nupkg")
-    |> Seq.iter (Shell.rm)
-}
+        !! (projDir @@ "/**/*.nupkg")
+        |> Seq.iter (Shell.rm)
+    }
 
-let build config =
-    !! (projDir @@ $"{projName}.fsproj")
-    |> string
-    |> DotNet.build (fun opts ->
-        { opts with
-            Configuration = config
-        }
-    )
+    let build config =
+         DotNet.build (fun opts ->
+            { opts with
+                Configuration = config
+            }
+         ) (projDir @@ $"{projName}.fsproj")
 
-let buildDebug = BuildTask.create "BuildDebug" [installDeps.IfNeeded; clean.IfNeeded] {
-    build DotNet.Debug
-}
+    let buildDebug = BuildTask.create "BuildDebug" [installDeps.IfNeeded; clean.IfNeeded] {
+        build DotNet.Debug
+    }
 
-let buildRelease = BuildTask.create "BuildRelease" [installDeps.IfNeeded; clean.IfNeeded] {
-    build DotNet.Release
-}
+    let buildRelease = BuildTask.create "BuildRelease" [installDeps.IfNeeded; clean.IfNeeded] {
+        build DotNet.Release
+    }
 
-let pack = BuildTask.create "Pack" [buildRelease] {
-    DotNet.exec id "paket" $"pack --symbols {projDir}" |> printRes "Pack"
-}
+    let pack = BuildTask.create "Pack" [buildRelease] {
+        DotNet.exec id "paket" $"pack --symbols {projDir}" |> printRes "Pack"
+    }
 
-let _all = BuildTask.createEmpty "All" [installDeps; clean; pack]
+    let _all = BuildTask.createEmpty "All" [installDeps; clean; pack]
 
-let defaultBuild = BuildTask.createEmpty "Build" [buildDebug]
+    let defaultBuild = BuildTask.createEmpty "Build" [buildDebug]
+
+    _all
 
 [<EntryPoint>]
 let main args =
-    BuildTask.setupContextFromArgv args
-    BuildTask.runOrDefaultApp _all
+    args
+    |> Array.toList
+    |> FakeExecutionContext.Create false (__SOURCE_DIRECTORY__ @@ __SOURCE_FILE__)
+    |> Fake
+    |> setExecutionContext
+    BuildTask.runOrDefaultApp (initTargets ())
